f52e9e0958956ad1d95e283c492361cc05a2db64
carlosesl1 <carlosedusl10@gmail.com>
2026-09-14 17:37:50 -0300

    seo: use deployed storefront URL in sitemap

--- storefront/app/sitemap.ts
+++ storefront/app/sitemap.ts
@@ -2,8 +2,7 @@ import type { MetadataRoute } from 'next'
 import { getProducts } from '@/lib/woocommerce/client'
 import { dedupeStoreProducts } from '@/lib/storefront/catalog'
 import { storefrontProductSlug } from '@/lib/woocommerce/presentation'
-
-const SITE_URL = (process.env.NEXT_PUBLIC_SITE_URL || 'https://housefindsstore.com').replace(/\/$/, '')
+import { SITE_URL } from '@/lib/storefront/site-url'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const staticPages: MetadataRoute.Sitemap = [
